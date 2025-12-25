67ccede22cf0a3a23b9725a6596d8fee2be0d4b4
Dipteshsingh <diptesh.singh73@gmail.com>
2025-12-25 15:52:41 +0530

    Update index.ts

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -11,6 +11,7 @@ connectDb();
 app.use(express.json());
 app.use(cors({
   origin: "http://localhost:3000",
+   "http://3.110.188.4:3001"
   credentials: true,
 }));
 const port = process.env.PORT || 5001;
