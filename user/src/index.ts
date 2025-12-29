c642d7e0cba97a2803bc551c4f0308bde9abe9e0
Dipteshsingh <diptesh.singh73@gmail.com>
2025-12-29 14:49:05 +0530

    Update index.ts

--- user/src/index.ts
+++ user/src/index.ts
@@ -21,7 +21,7 @@ redisClient.connect()
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", "http://3.109.2.127:3000", "http://3.109.2.127:3002"];
+const allowedOrigins = ["http://localhost:3000", "http://65.0.19.85:3000", "http://65.0.19.85:3002"];
 
 app.use(cors({
   origin: function(origin, callback) {
