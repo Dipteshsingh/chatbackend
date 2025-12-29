76611d94ac5f16837291496c586282c8512c9c6e
Dipteshsingh <diptesh.singh73@gmail.com>
2025-12-29 14:49:36 +0530

    Update index.ts

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -7,7 +7,7 @@ import { app, server } from './config/socket.js';
 dotenv.config();
 
 connectDb();
-const allowedOrigins = ["http://localhost:3000", "http://3.109.2.127:3000", "http://3.109.2.127:3002"];
+const allowedOrigins = ["http://localhost:3000", "http://65.0.19.85:3000", "http://65.0.19.85:3002"];
 
 app.use(cors({
   origin: function(origin, callback) {
